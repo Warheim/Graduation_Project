965884ab274c74701fa02129bca0647ee17ff2f2
warheim <warheim24@gmail.com>
2023-05-23 22:43:48 +0300

    starting models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from orders.settings import USER_TYPE_CHOICES
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Shop(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Product(models.Model):
+    pass
+
+
+class ProductInfo(models.Model):
+    pass
+
+
+class Parameter(models.Model):
+    pass
+
+
+class ProductParameter(models.Model):
+    pass
+
+
+class Order(models.Model):
+    pass
+
+
+class OrderProduct(models.Model):
+    pass
+
+
+class Buyer(models.Model):
+    pass
