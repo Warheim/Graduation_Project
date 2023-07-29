a5f408064f00e494f18e36528552d92a59994b42
warheim <warheim24@gmail.com>
2023-07-29 21:07:27 +0300

    making paths and requests

--- backend/urls.py
+++ backend/urls.py
@@ -4,5 +4,5 @@ from backend.views import ShopUpdate
 
 app_name = 'backend'
 urlpatterns = [
-    path('shop/update', ShopUpdate.as_view(), name='shop-update'),
+    path('update', ShopUpdate.as_view(), name='shop-update'),
 ]
