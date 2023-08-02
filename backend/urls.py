a9f061eaa8730a2b71b31665a932614d5d516d8a
warheim <warheim24@gmail.com>
2023-08-02 10:45:53 +0300

    celery + celery config added, test views deleted

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,4 @@
 from django.urls import path
 
-from backend.views import ShopUpdate
-
 app_name = 'backend'
-urlpatterns = [
-    path('shop/update', ShopUpdate.as_view(), name='shop-update'),
-]
+urlpatterns = []
